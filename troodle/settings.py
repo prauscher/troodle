7dc31ecb46e1f4851c9c0c56340cee0ecec48ce9
Patrick Rauscher <prauscher@prauscher.de>
2019-10-08 11:36:28 +0200

    move settings DEBUG, ALLOWED_HOSTS to secret_settings

--- troodle/settings.py
+++ troodle/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
-from .secret_settings import SECRET_KEY, DATABASES
+from .secret_settings import SECRET_KEY, DATABASES, DEBUG, ALLOWED_HOSTS
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
 
 # Application definition
 

93dc72f2b36d311cbffd138ec84244f00dba28fb
Patrick Rauscher <prauscher@prauscher.de>
2019-10-12 12:33:12 +0200

    allow secret_settings to contain more settings

--- troodle/settings.py
+++ troodle/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
-from .secret_settings import SECRET_KEY, DATABASES, DEBUG, ALLOWED_HOSTS, EMAIL_BACKEND
+from .secret_settings import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))

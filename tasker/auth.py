abbfb9303dd4d1f4e07d129f37b26887d8e52acd
Patrick Rauscher <prauscher@prauscher.de>
2020-03-14 11:47:33 +0100

    store participants in dict instead of multiple keys in session, used later to store push subscriptions

--- tasker/auth.py
+++ tasker/auth.py
@@ -17,15 +17,17 @@ class AuthBoardMixin:
         return context
 
 
-def get_session_key(board):
-    return 'participant{}'.format(board.id)
+def _prepare_session(request):
+    if 'participant' not in request.session:
+        request.session['participant'] = {}
 
 
 def get_participant(request, board):
-    if get_session_key(board) not in request.session:
+    _prepare_session(request)
+    if str(board.id) not in request.session['participant']:
         raise NotLoggedInException
 
-    participant = models.Participant.objects.get(id=request.session['participant{}'.format(board.id)])
+    participant = models.Participant.objects.get(id=request.session['participant'][str(board.id)])
     assert participant.board == board, "Stored Participant has invalid board"
 
     return participant
@@ -40,8 +42,13 @@ def apply_context(request, board, context):
 
 def login(request, board, nick):
     participant, _ = models.Participant.objects.get_or_create(nick=nick, board=board)
-    request.session[get_session_key(board)] = participant.id
+
+    _prepare_session(request)
+    request.session['participant'][str(board.id)] = participant.id
+    request.session.modified = True
 
 
 def logout(request, board):
-    del request.session[get_session_key(board)]
+    _prepare_session(request)
+    del request.session['participant'][str(board.id)]
+    request.session.modified = True
